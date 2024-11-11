9ef4bc40145a3d79e12c55168de5eddda6cdbf66
MacBook Pro <almir.mustafic.tz@gmail.com>
2024-11-11 22:17:59 +0100

    ias1-fcai3-pip3

--- IAS1/Assignment1/ex2student.py
+++ IAS1/Assignment1/ex2student.py
@@ -3,6 +3,7 @@ import vrep
 import numpy as np
 import time
 import math
+
 # Command to run
 # C:\Users\root\AppData\Local\Microsoft\WindowsApps\python.exe .\ex2student.py
 # Robot Geometry
@@ -10,6 +11,10 @@ diameter         = 0.1                 # Diameter of the wheels in m
 perimeter        = math.pi * diameter  # Perimeter of the wheels in m
 wheelDistanceVer = 0.471               # Vertical distance between the wheels
 wheelDistanceHor = 0.30046             # Horizontal distance between the wheels
+
+
+def closeServerCommunication(clientId):
+    vrep.simxFinish(clientId)
 
 
 def setWheelVelocityToZero(wheelJoints, clientID):
@@ -32,10 +37,10 @@ def moveForward(distance, speed, clientID, wheelJoints):
     controlCommunication(clientID, True)
 
     # set wheel velocity to calculated values (hint: pause communication in the end)
-    for i in range(4):
-        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], wheel_speeds[i], vrep.simx_opmode_oneshot)
+    for i, wheelJoint in enumerate(wheelJoints):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoint, wheel_speeds[i], vrep.simx_opmode_oneshot)
 
-    controlCommunication(clientID, False)  
+    controlCommunication(clientID, False)
     required_sleep_time = (distance/perimeter) * (2*math.pi/speed) * correctionFactor
     time.sleep(required_sleep_time)
 
@@ -52,8 +57,8 @@ def turnRight(degree, speed, clientID, wheelJoints):
     controlCommunication(clientID, True)
 
     # set wheel velocity to calculated values
-    for i in range(4):
-        vrep.simxSetJointTargetVelocity(clientID, wheelJoints[i], wheel_speeds[i], vrep.simx_opmode_oneshot)
+    for i, wheelJoint in enumerate(wheelJoints):
+        vrep.simxSetJointTargetVelocity(clientID, wheelJoint, wheel_speeds[i], vrep.simx_opmode_oneshot)
 
     controlCommunication(clientID, False)
     
@@ -70,41 +75,43 @@ def wheelVel(forwBackVel , leftRightVel , rotVel):
     rearLeft = forwBackVel - leftRightVel - rotVel
     rearRight = forwBackVel + leftRightVel + rotVel
     frontRight = forwBackVel + leftRightVel + rotVel
+    print(f"Front Left: {frontLeft}, Rear Left: {rearLeft}, Rear Right: {rearRight}, Front Right: {frontRight}")
+
     return np.array([frontLeft, rearLeft, rearRight, frontRight])
 
 
 def main():
-    # connect to coppeliasim
+    # connect to CoppeliaSim
     print ('Program started')
-    vrep.simxFinish(-1) 
-    clientID=vrep.simxStart('127.0.0.1',19997,True,True, 2000,5)
+    # Close previous unclosed or leftover connections
+    # closeServerCommunication(-1)
+    clientID = vrep.simxStart('127.0.0.1',19997,True,True, 2000,5)
 
-    if clientID!=-1:
+    if clientID != -1:
         print ('Connected to remote API server')
-        emptyBuff = bytearray()
 
         # Start the simulation:
         vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot_wait)
 
-        # initiaize robot
-        # Retrieve wheel joint handles:
-        wheelJoints = np.empty(4, dtype=int)
-        wheelJoints.fill(-1)  # front left, rear left, rear right, front right
-        joint_names = ['rollingJoint_fl', 'rollingJoint_rl', 'rollingJoint_rr', 'rollingJoint_fr']
+        # Initialize robot and retrieve wheel joint handles:
+        wheelJoints = []
 
-        for i, joint_name in enumerate(joint_names):
-            res, wheelJoints[i] = vrep.simxGetObjectHandle(clientID, joint_name, vrep.simx_opmode_oneshot_wait)
-
-        armJointsHandle = [0] * 5
+        for joint_name in ['rollingJoint_fl', 'rollingJoint_rl', 'rollingJoint_rr', 'rollingJoint_fr']:
+            res, joint_handle = vrep.simxGetObjectHandle(clientID, joint_name, vrep.simx_opmode_oneshot_wait)
+            wheelJoints.append(joint_handle)
         
         #--------------------------------------------------arm------------------------------------------------------------------
         # Retrieve arm joint handles
-        for i in range(0, 5):
-            res,armJointsHandle[i] = vrep.simxGetObjectHandle(clientID, 'youBotArmJoint%d' % i, vrep.simx_opmode_oneshot_wait)
-        
-        # Move the third joint of the arm four times
+        armJointsHandle = []
+
+        for joint_name in ['youBotArmJoint0', 'youBotArmJoint1', 'youBotArmJoint2', 'youBotArmJoint3',
+                           'youBotArmJoint4']:
+            res, joint_handle = vrep.simxGetObjectHandle(clientID, joint_name, vrep.simx_opmode_oneshot_wait)
+            armJointsHandle.append(joint_handle)
+
+        # Move the third joint of the arm four times: math.pi / 8 is cca. 0.3927 => each loop is + 0.3927 => cca 22.5 degrees
         for i in range(0, 4):
-            res = vrep.simxSetJointTargetPosition(clientID, armJointsHandle[2], i*math.pi/8, vrep.simx_opmode_oneshot)
+            vrep.simxSetJointTargetPosition(clientID, armJointsHandle[2], i*math.pi/8, vrep.simx_opmode_oneshot)
             time.sleep(1)
         #-----------------------------------------------------------------------------------------------------------------------
         
@@ -122,7 +129,7 @@ def main():
 
         controlCommunication(clientID, False)
         vrep.simxStopSimulation(clientID, vrep.simx_opmode_oneshot_wait)
-        vrep.simxFinish(clientID)
+        closeServerCommunication(clientID)
     else:
         print("Connection to the remote API server failed! Re-open CoppeliaSim, load the movement.ttt and run this script again.")
     print("Program ended")
