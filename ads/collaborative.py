d05af4d25e97b78ade8995b4b149960e17105fbe
Almir Mustafic <almir.mustafic.tz@gmail.com>
2025-11-30 23:15:06 +0100

    pick-users-for-movies-refactored

--- ads/collaborative.py
+++ ads/collaborative.py
@@ -42,27 +42,54 @@ class CollaborativeRecommender:
         
         return total_score
     
-    def _pick_users_for_movies(self, similar_users, recommended_movie_ids, max_users_per_movie):
-        users = []
-        used = set()
-        for mid in recommended_movie_ids:
-            movie_users = [uid for uid in similar_users if uid in self.graph.get_movie_users(mid)]
-            picked = []
-            for uid in movie_users:
-                if len(picked) >= max_users_per_movie:
-                    break
-                if uid not in used:
-                    picked.append(uid)
-                    used.add(uid)
-            if len(picked) < max_users_per_movie:
-                for uid in movie_users:
-                    if len(picked) >= max_users_per_movie:
-                        break
-                    if uid not in picked:
-                        picked.append(uid)
-            users.extend(picked)
-        return users
     
+    def pick_users_for_movies(self, similar_users, recommended_movie_ids, max_users_per_movie):
+        all_picked_users = []
+        
+        # Here we just pick users to show with movies in the graph (not for recommendations).
+        # Track which users have already been selected to avoid duplicates (if possible)
+        # across different movies.
+        used_users = set()
+        
+        # For each recommended movie, pick users who watched it.
+        # Retain users from the similar_users set only (collaborative filtering).
+        for movie_id in recommended_movie_ids:
+            users_who_watched_movie = self.graph.get_movie_users(movie_id)
+            relevant_users = []
+
+            for user_id in similar_users:
+                if user_id in users_who_watched_movie:
+                    relevant_users.append(user_id)
+            
+            # First, pick users we haven't shown for other movies yet
+            # (this makes the graph show different users for each movie).
+            picked_for_this_movie = []
+
+            # From relevant users keep those that were not used with other
+            # movies yet, and stop if the maximum number
+            # of users per movie reached.
+            for user_id in relevant_users:
+                if len(picked_for_this_movie) >= max_users_per_movie:
+                    break
+                
+                if user_id not in used_users:
+                    picked_for_this_movie.append(user_id)
+                    used_users.add(user_id)
+            
+            # If not enough users for this movie, add already-used users.
+            if len(picked_for_this_movie) < max_users_per_movie:
+                for user_id in relevant_users:
+                    if len(picked_for_this_movie) >= max_users_per_movie:
+                        break
+                    
+                    if user_id not in picked_for_this_movie:
+                        picked_for_this_movie.append(user_id)
+            
+            all_picked_users.extend(picked_for_this_movie)
+        
+        return all_picked_users
+    
+
     def recommend(self, user_id, top_k=10):
         user_movies = self.graph.get_user_movies(user_id)
         
@@ -80,7 +107,7 @@ class CollaborativeRecommender:
         recommendations = scores[:top_k]
         
         recommended_movie_ids = [movie_id for movie_id, _ in recommendations]
-        similar_users_for_viz = self._pick_users_for_movies(similar_users, recommended_movie_ids, MAX_SIMILAR_USERS)
+        similar_users_for_viz = self.pick_users_for_movies(similar_users, recommended_movie_ids, MAX_SIMILAR_USERS)
         
         return recommendations, recommended_movie_ids, similar_users_for_viz
 
